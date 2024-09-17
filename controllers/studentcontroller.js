10f042944e592538b81792b740d3ca6e7ab241e0
kanakbhardwaj12 <shivombhardwaj2424@gmail.com>
2024-09-17 16:55:41 +0530

    hello

--- controllers/studentcontroller.js
+++ controllers/studentcontroller.js
@@ -21,7 +21,7 @@ async function getStudents(req,res) {
 }
 async function getStudentByRollNo(req,res) {
     try{
-        let rollNo = pasrseInt(req.params.rollNo);
+        let rollNo = parseInt(req.params.rollNo);
         let student = await Student.findOne({ rollNo:rollNo });
         console.log(student,'student');
         res.send(student)
